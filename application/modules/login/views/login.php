fdeb4cff672c7064f01c59dacbadf20c47cd4917
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2020-11-18 19:12:57 -0700

    Se cargan imagenes y se muestra en la vista de login el favicon ico

--- application/modules/login/views/login.php
+++ application/modules/login/views/login.php
@@ -10,6 +10,7 @@
     <meta name="author" content="">
 	
     <title>JBB</title>
+	<link rel="icon" type="image/png" href="<?php echo base_url("images/favicon.ico"); ?>" />
 
     <!-- Bootstrap Core CSS -->
 	<link href="<?php echo base_url("assets/bootstrap/vendor/bootstrap/css/bootstrap.min.css"); ?>" rel="stylesheet">
