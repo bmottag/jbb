3dbe287a1cfd132def4660f18f354dea0a97de4f
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2021-01-21 13:50:28 -0500

    Equipos-Combustible: Ajuste de forma para las validaciones

--- application/modules/equipos/views/combustible_modal.php
+++ application/modules/equipos/views/combustible_modal.php
@@ -14,14 +14,14 @@
 		<div class="row">
 			<div class="col-sm-6">		
 				<div class="form-group text-left">
-					<label for="kilometros_actuales">Horas o Kilometros Actuales: *</label>
+					<label class="control-label" for="kilometros_actuales">Horas o Kilometros Actuales: *</label>
 					<input type="text" id="kilometros_actuales" name="kilometros_actuales" class="form-control" value="<?php echo $information?$information[0]["kilometros_actuales"]:""; ?>" placeholder="Horas o Kilometros Actuales" >
 				</div>
 			</div>
 		
 			<div class="col-sm-6">		
 				<div class="form-group text-left">
-					<label for="id_operador">Operador: *</label>
+					<label class="control-label" for="id_operador">Operador: *</label>
 					<select name="id_operador" id="id_operador" class="form-control" required>
 						<option value=''>Seleccione...</option>
 						<?php for ($i = 0; $i < count($listaOperadores); $i++) { ?>
@@ -35,7 +35,7 @@
 		<div class="row">
 			<div class="col-sm-6">		
 				<div class="form-group text-left">
-					<label for="tipo_consumo">Tipo de Consumo: *</label>
+					<label class="control-label" for="tipo_consumo">Tipo de Consumo: *</label>
 					<select name="tipo_consumo" id="tipo_consumo" class="form-control" required>
 						<option value=''>Seleccione...</option>
 						<option value=1 <?php if($information && $information[0]["tipo_consumo"] == 1) { echo "selected"; }  ?>>Combustible</option>
@@ -49,7 +49,7 @@
 		
 			<div class="col-sm-6">		
 				<div class="form-group text-left">
-					<label for="cantidad">Cantidad: <small>(En Galones)</small>*</label>
+					<label class="control-label" for="cantidad">Cantidad: <small>(En Galones)</small>*</label>
 					<input type="text" id="cantidad" name="cantidad" class="form-control" value="<?php echo $information?$information[0]["cantidad"]:""; ?>" placeholder="Cantidad" >
 				</div>
 			</div>
@@ -58,14 +58,14 @@
 		<div class="row">
 			<div class="col-sm-6">		
 				<div class="form-group text-left">
-					<label for="valor">Valor: *</label>
+					<label class="control-label" for="valor">Valor: *</label>
 					<input type="text" id="valor" name="valor" class="form-control" value="<?php echo $information?$information[0]["valor"]:""; ?>" placeholder="Valor" >
 				</div>
 			</div>
 
 			<div class="col-sm-6">		
 				<div class="form-group text-left">
-					<label for="labor_realizada">Labor realizada: *</label>
+					<label class="control-label" for="labor_realizada">Labor realizada: *</label>
 					<textarea id="labor_realizada" name="labor_realizada" placeholder="Labor realizada" class="form-control" rows="3"><?php echo $information?$information[0]["labor_realizada"]:""; ?></textarea>
 				</div>
 			</div>
