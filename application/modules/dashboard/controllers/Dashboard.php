e3f2492c61ecef90def16b57d10850cfe93e3291
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2021-01-21 12:31:22 -0500

    Dashboard: Se ajusta la vista. Falta ajustar enlaces

--- application/modules/dashboard/controllers/Dashboard.php
+++ application/modules/dashboard/controllers/Dashboard.php
@@ -7,43 +7,6 @@ class Dashboard extends CI_Controller {
         parent::__construct();
 		$this->load->model("dashboard_model");
     }
-
-	/**
-	 * Index Page for this controller.
-	 * Basic dashboard
-	 */
-	public function index()
-	{	
-			//cuenta payroll para el usuario 
-			$arrParam["task"] = 1;//buscar por timestap
-			$data['noTareas'] = $this->general_model->countTask($arrParam);
-			
-			$data['noSafety'] = $this->dashboard_model->countSafety();//cuenta registros de safety
-			$data['noJobs'] = $this->dashboard_model->countJobs();//cuenta registros de Jobs
-			$data['noHauling'] = $this->dashboard_model->countHauling();//cuenta registros de hauling
-			$data['noDailyInspection'] = $this->dashboard_model->countDailyInspection();//cuenta registros de DailyInspection
-			$data['noHeavyInspection'] = $this->dashboard_model->countHeavyInspection();//cuenta registros de HeavyInspection
-			$data['noSpecialInspection'] = $this->dashboard_model->countSpecialInspection();//cuenta registros de SpecialInspection
-			
-			//informacion de un dayoff si lo aprobaron y lo negaron
-			$data['dayoff'] = $this->dashboard_model->dayOffInfo();
-				
-			$data['infoMaintenance'] = $this->general_model->get_maintenance_check();
-						
-			$arrParam["limit"] = 30;//Limite de registros para la consulta
-			$data['info'] = $this->general_model->get_task($arrParam);//search the last 5 records 
-			
-			$data['infoSafety'] = $this->general_model->get_safety($arrParam);//info de safety
-			
-			$arrParam["limit"] = 6;//Limite de registros para la consulta
-			$data['infoWaterTruck'] = $this->general_model->get_special_inspection_water_truck($arrParam);//info de water truck
-			$data['infoHydrovac'] = $this->general_model->get_special_inspection_hydrovac($arrParam);//info de hydrovac
-			$data['infoSweeper'] = $this->general_model->get_special_inspection_sweeper($arrParam);//info de sweeper
-			$data['infoGenerator'] = $this->general_model->get_special_inspection_generator($arrParam);//info de generador
-		
-			$data["view"] = "dashboard";
-			$this->load->view("layout", $data);
-	}
 		
 	/**
 	 * SUPER ADMIN DASHBOARD
@@ -51,7 +14,23 @@ class Dashboard extends CI_Controller {
 	public function admin()
 	{				
 			$data = array();
-		
+
+			//Tipo -> vehiculos
+			$arrParam = array(
+				"idTipoEquipo" => 1,
+				"estadoEquipo" => 1
+			);
+			$infoVehiculos = $this->general_model->get_equipos_info($arrParam);//info de vehiculos
+			$data['noVehiculos'] = $infoVehiculos?count($infoVehiculos):0;
+
+			//Tipo -> Bombas
+			$arrParam = array(
+				"idTipoEquipo" => 2,
+				"estadoEquipo" => 1
+			);
+			$infoBombas = $this->general_model->get_equipos_info($arrParam);//info de bombas
+			$data['noBombas'] = $infoBombas?count($infoBombas):0;
+			
 			$data["view"] = "dashboard";
 			$this->load->view("layout", $data);
 	}
--- application/modules/dashboard/views/dashboard.php
+++ application/modules/dashboard/views/dashboard.php
@@ -1,18 +1,18 @@
 <a name="anclaUp"></a>
 
-        <div id="page-wrapper">
-            <div class="row"><br>
-				<div class="col-md-12">
-					<div class="panel panel-primary">
-						<div class="panel-heading">
-							<h4 class="list-group-item-heading">
-								DASHBOARD
-							</h4>
-						</div>
-					</div>
+<div id="page-wrapper">
+    <div class="row"><br>
+		<div class="col-md-12">
+			<div class="panel panel-primary">
+				<div class="panel-heading">
+					<h4 class="list-group-item-heading">
+						DASHBOARD
+					</h4>
 				</div>
-				<!-- /.col-lg-12 -->
-            </div>
+			</div>
+		</div>
+		<!-- /.col-lg-12 -->
+    </div>
 								
 <?php
 $retornoExito = $this->session->flashdata('retornoExito');
@@ -44,107 +44,104 @@ if ($retornoError) {
 }
 ?> 
 			
-			
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-book fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">10</div>
-                                    <div>Registros</div>
-                                </div>
-                            </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-book fa-5x"></i>
                         </div>
-						
-                        <a href="#anclaPayroll">
-                            <div class="panel-footer">
-                                <span class="pull-left">Últimos registros</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge">10</div>
+                            <div>Ordenes de Trabajo</div>
+                        </div>
                     </div>
                 </div>
 				
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-amarello">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-life-saver fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">20</div>
-                                    <div>Vehículos</div>
-                                </div>
-                            </div>
+                <a href="#anclaPayroll">
+                    <div class="panel-footer">
+                        <span class="pull-left">Últimos registros</span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+		
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-green">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-life-saver fa-5x"></i>
                         </div>
-
-                        <a href="#anclaSafety">
-                            <div class="panel-footer">
-                                <span class="pull-left"> Últimos registros </span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo $noVehiculos; ?></div>
+                            <div>Vehículos</div>
+                        </div>
                     </div>
                 </div>
 
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-purpura">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-briefcase fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">30</div>
-                                    <div>Mantenimientos</div>
-                                </div>
-                            </div>
-                        </div>
+                <a href="#anclaSafety">
+                    <div class="panel-footer">
+                        <span class="pull-left"> Ver registros </span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
 
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left"> Últimos registros </span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-yellow">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-briefcase fa-5x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge"><?php echo $noBombas; ?></div>
+                            <div>Bombas</div>
+                        </div>
                     </div>
                 </div>
 
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-yellow">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-truck fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">100</div>
-                                    <div>.</div>
-                                </div>
-                            </div>
-                        </div>
+                <a href="#">
+                    <div class="panel-footer">
+                        <span class="pull-left"> Ver registros </span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
 
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">Últimos registros </span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
+        <div class="col-lg-3 col-md-6">
+            <div class="panel panel-red">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fa fa-truck fa-5x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <div class="huge">0</div>
+                            <div>Maquinas</div>
+                        </div>
                     </div>
                 </div>
 
-			</div>
-			
-	
+                <a href="#">
+                    <div class="panel-footer">
+                        <span class="pull-left">NO HAY REGISTROS </span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
 
+	</div>
 
+</div>
